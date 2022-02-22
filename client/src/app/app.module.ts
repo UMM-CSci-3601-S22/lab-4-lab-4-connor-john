fb812f71ba8b3ff731a8d256f97a4263a42cd2cd
Conner <hetti031@morris.umn.edu>
2022-02-22 15:47:55 -0600

    Create add-todo component
    Co-authored-by: jpwalbran <walbr042@morris.umn.edu>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { UserCardComponent } from './users/user-card.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { UserService } from './users/user.service';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -60,6 +61,7 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     AddUserComponent,
+    AddTodoComponent,
   ],
   imports: [
     BrowserModule,
