ff2741659a5603cac28762a0745f64f6c9892e0e
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-02-25 15:06:05 -0600

    Change comment formatting
    Changed the top level comment formatting on read-from-file.ts to make github checks rerun.

--- client/cypress/support/read-from-file.ts
+++ client/cypress/support/read-from-file.ts
@@ -1,9 +1,11 @@
 import { fstat, readFile } from 'fs';
 
 export class ReadFromFile {
-  // a basic file reader object to enable loading large test files into strings
-  // this should be helpful when we want to test that large text-boxes don't allow us to enter
-  // absurdly long inputs.
+  /*
+   *A basic file reader object to enable loading large test files into strings
+   *this should be helpful when we want to test that large text-boxes don't allow us to enter
+   *absurdly long inputs.
+  */
 
   private output: string;
 
